b899cb8a2c3cb282f7ce466e54a0c90dde75804e
Egor <enotniy@gmail.com>
2015-09-17 22:24:20 +0600

    Inital commit

---
+++ Auto Engineering Calculator/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auto_Engineering_Calculator
+{
+    public class Car
+    {
+        private string Model;
+        public Car(string Model) 
+        {
+            this.Model = Model;
+        }
+    }
+}
